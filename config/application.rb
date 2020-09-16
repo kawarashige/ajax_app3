ce77e827a4d3b10e2fc8cf3cc272158f1ecd7697
kawarashige <kawarashige23@gmail.com>
2020-09-17 04:06:38 +0900

    エラーの解消

--- config/application.rb
+++ config/application.rb
@@ -4,7 +4,6 @@ require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-ActiveSupport::Deprecation.silenced = true if Rails.version == '6.0.3.2'
 Bundler.require(*Rails.groups)
 
 module FirstApp
