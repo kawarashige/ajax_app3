a6af3cc0c3c60cac6d914b35355cc29ee9992b76
kawarashige <kawarashige23@gmail.com>
2020-09-17 03:44:17 +0900

    heroku上の環境変数の値を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,9 @@ class ApplicationController < ActionController::Base
 
   def basic_auth
     authenticate_or_request_with_http_basic do |username, password|
-      username == 'ENV["BASIC_AUTH_USER"]' && password == 'BASIC_AUTH_PASSWORDherokuheroku'
+      username == 'ENV["BASIC_AUTH_USER"]' && password == 'BASIC_AUTH_PASSWORDherokuheroku-""Bにんsっっyにんsっっy
+      
+      '
     end
   end
 end
